b2b7ae8a198a77e6a398fe174adca5ec05919758
Amir Aziz <amir.zezo2001@gmail.com>
2024-11-04 14:23:15 +0500

    site map and partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,30 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import keystatic from '@keystatic/astro';
+import keystatic from "@keystatic/astro";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import netlify from "@astrojs/netlify";
-
+import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://moorhouse2024.netlify.app",
-  integrations: [mdx(), sitemap(), tailwind(), react(), markdoc(), keystatic(), robotsTxt()],
+  site: "https://moorhousecoating.com",
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => page == "https://moorhousecoating.com/keystatic/",
+    }),
+    ,
+    tailwind(),
+    react(),
+    markdoc(),
+    keystatic(),
+    robotsTxt(),
+    partytown(),
+  ],
   output: "hybrid",
-  adapter: netlify()
+  adapter: netlify(),
 });
